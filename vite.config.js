d6e1f47b2595d7e655fc9a3682e6ee73fdd379b3
Douye <mpodeke@gmail.com>
2024-11-11 19:37:00 +0100

    updated outdir to 'build'

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'public',
   build: {
-    outDir: '../dist',  // Output directory outside of `public` for the final build
+    outDir: 'build',  
   },
 });
