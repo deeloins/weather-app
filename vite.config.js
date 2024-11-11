3d78b6e5be9bab3651a4d59c4803dab7201bb1a6
Douye <mpodeke@gmail.com>
2024-11-11 19:08:06 +0100

    modified vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: 'public',
   server: {
     historyApiFallback: true, // This helps handle client-side routing
   },
