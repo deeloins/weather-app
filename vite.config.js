4e99872a357eafb03dd2b729b7133654df1106c6
Douye <mpodeke@gmail.com>
2024-11-11 19:14:44 +0100

    "Remove react plugin and server config, add build config with outDir"

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
   root: 'public',
-  server: {
-    historyApiFallback: true, // This helps handle client-side routing
+  build: {
+    outDir: '../dist',  // Output directory outside of `public` for the final build
   },
 });
